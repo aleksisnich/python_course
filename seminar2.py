02b91fb846e758a54ef24be4755de184c10ce92c
aleksis <nichik_a@bk.ru>
2024-04-27 19:58:35 +0300

    complete task 2

--- seminar2.py
+++ seminar2.py
@@ -1,15 +1,35 @@
-# Первое задание. Вычислить значение n! при n > 0. Использовать цикл while.
+# # Первое задание. Вычислить значение n! при n > 0. Использовать цикл while.
 
-n = int(input('Введите целое число: '))
-i = 2
-n_factorial = 1
-if n > 0:
-    while i <= n:
-        n_factorial = n_factorial * i
-        i = i + 1
-    print('n! = ', n_factorial)
-elif n == 0:
-        n_factorial = 1
-        print('n! = ', n_factorial)
+# n = int(input('Введите целое число: '))
+# i = 2
+# n_factorial = 1
+# if n > 0:
+#     while i <= n:
+#         n_factorial = n_factorial * i
+#         i = i + 1
+#     print('n! = ', n_factorial)
+# elif n == 0:
+#         n_factorial = 1
+#         print('n! = ', n_factorial)
+# else:
+#     print("Число не может быть отрицательным")
+
+# Второе задание. Дано натуральное число A > 1. Определите, каким по счету числом Фибоначчи оно является, 
+# то есть выведите такое число n, что фи(n) = A. Если A не является числом Фибоначчи, выведите число -1.
+
+n = int(input('Введите число: '))
+fib = 0
+i = 1
+count = 1
+
+while fib < n:
+    c = fib + i
+    fib = i
+    i = c 
+    count = count + 1
+
+if n == fib:
+    print('Порядковый номер числа Фибоначчи:', count)
 else:
-    print("Число не может быть отрицательным")
+    print('-1')
+
