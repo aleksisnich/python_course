38709f7904756e4110dc37d0f7696731869ec823
aleksis <nichik_a@bk.ru>
2024-05-06 23:29:32 +0300

    addc task to seminar 3

--- seminar3.py
+++ seminar3.py
@@ -18,21 +18,102 @@
 # Дана последовательность из N целых чисел и число K. 
 # Необходимо сдвинуть всю последовательность (сдвиг-циклический) на K элементов вправо, k > 0.
 
-list = []
-newlist =[]
-n = int(input('Введите количество чисел в списке: '))
-for i in range(n):
-    newlist.append(i)
-for i in range(n):
-    list.append(int(input("Введите любое число: ")))
+# list = []
+# newlist =[]
+# n = int(input('Введите количество чисел в списке: '))
+# for i in range(n):
+#     newlist.append(i)
+# for i in range(n):
+#     list.append(int(input("Введите любое число: ")))
     
-k = int(input('Введите число циклического сдвига: '))
+# k = int(input('Введите число циклического сдвига: '))
 
-for i in range(n):
-    if i + k < n:
-        newlist[i + k] = list[i]
-    else:
-        c = k - (n - i)
-        newlist[c] = list[i] 
-print(newlist)
+# for i in range(n):
+#     if i + k < n:
+#         newlist[i + k] = list[i]
+#     else:
+#         c = k - (n - i)
+#         newlist[c] = list[i] 
+# print(newlist)
+
+
+# Искусственный интеллект Антон, созданный Гилфойлом, взломал сеть умных холодильников. 
+# Теперь он использует их в качестве серверов "Пегого дудочника". 
+# Помогите владельцу фирмы отыскать все зараженные холодильники.
+
+# Для каждого холодильника существует строка с данными, состоящая из строчных букв и цифр, 
+# и если в ней присутствует слово "anton" (необязательно рядом стоящие буквы, 
+# главное наличие последовательности букв), то холодильник заражен и нужно вывести номер холодильника, 
+# нумерация начинается с единицы
+
+# Формат входных данных
+# В первой строке подаётся число n – количество холодильников. В последующих строках вводятся строки, 
+# содержащие латинские строчные буквы и цифры, в каждой строке от 5 до 100 символов.
+
+# Формат выходных данных
+# Программа должна вывести номера зараженных холодильников через пробел. 
+# Если таких холодильников нет, ничего выводить не нужно.
+
+
+# Sample Input 1:
+# 6
+# 222anton456
+# a1n1t1o1n1
+# 0000a0000n00t00000o000000n
+# gylfole
+# richard
+# ant0n
+# Sample Output 1: 
+# 1 2 3
+
+# Sample Input 2:
+# 9
+# osfjwoiergwoignaewpjofwoeijfnwfonewfoignewtowenffnoeiwowjfninoiwfen
+# anton
+# aoooooooooontooooo
+# elelelelelelelelelel
+# ntoneeee
+# tonee
+# 253235235a5323352n25235352t253523523235oo235523523523n
+# antoooooooooooooooooooooooooooooooooooooooooooooooooooon
+# unton
+# Sample Output 2:
+# 1 2 7 8
         
+        
+# bag = 'anton'
+# result = []
+# n = int(input('Введите количество холодильников: '))
+# # for i in range(n):
+# #     frozen.append(input("Введите шифр для каждого холодильника: "))
+# # print(len(frozen))
+# for i in range(n):
+#     frozen = input("Введите шифр для каждого холодильника: ")
+#     res = ''
+#     for j in bag:
+#         #print(j)
+#         if j in frozen:
+#             res = res + j
+#     # print(res)
+#     if res == 'anton':
+#         result.append(i + 1)
+# print(*result)
+
+# ВТОРОЙ СПОСОБ        
+frozen = []
+bag = 'anton'
+result = []
+string = ''
+n = int(input('Введите количество холодильников: '))
+for i in range(n):
+    frozen.append(input("Введите шифр для каждого холодильника: "))
+#print(len(frozen))
+for i in range(n):
+    string = frozen[i]
+    res = ''
+    for j in bag:
+        if j in string:
+            res = res + j
+    if res == bag:
+        result.append(i + 1)
+print(*result)
