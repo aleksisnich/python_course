25efbe6da76b1b98a7495a94df2b29f637a92ed5
aleksis <nichik_a@bk.ru>
2024-06-12 01:03:13 +0300

    add to seminar 8

--- seminar7.py
+++ seminar7.py
@@ -151,7 +151,20 @@
 #     if calc == (len(check)-1):
 #         print('Парам пам-пам')
             
-            
+# vowels = ['а', 'е', 'ё', 'и', 'й', 'о', 'у', 'ы', 'э', 'ю', 'я']
+# phrases = stroka.split()
+# if len(phrases) < 2:
+#     print('Количество фраз должно быть больше одной!')
+# else:
+#     countVowels = []
+
+#     for i in phrases:
+#         countVowels.append(len([x for x in i if x.lower() in vowels]))
+
+#     if countVowels.count(countVowels[0]) == len(countVowels):
+#         print('Парам пам-пам')
+#     else:
+#         print('Пам парам')        
 
     
 
