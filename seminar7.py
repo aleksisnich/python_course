0f8ddc7d0f1c859b3ca2444debe8382809db55dd
aleksis <nichik_a@bk.ru>
2024-06-11 09:36:14 +0300

    moderated seminar 7

--- seminar7.py
+++ seminar7.py
@@ -1,3 +1,84 @@
+# Задача №47. Решение в группах
+# У вас есть код, который вы не можете менять (так часто бывает, когда код в глубине
+# программы используется множество раз и вы не хотите ничего сломать):
+# transformation = <???>
+# values = [2, 3, 5, 7, 11, 13, 17, 19, 23, 29] # или любой другой список
+# transormed_values = list(map(transformation, values))
+# Единственный способ вашего взаимодействия с этим кодом - посредством задания
+# функции transformation.
+# Однако вы поняли, что для вашей текущей задачи вам не нужно никак преобразовывать
+# список значений, а нужно получить его как есть.
+# Напишите такое лямбда-выражение transformation, чтобы transformed_values получился
+# копией values.
+# Пример ввода и вывода данных представлены на следующем
+# слайде
+# Ввод:
+# values = [1, 23, 42, ‘asdfg’]
+# transformed_values = list(map(trasformation, values))
+# if values == transformed_values:
+# print(‘ok’)
+# else:
+# print(‘fail’)
+# Вывод:
+# ok
+
+
+# transformation = lambda x: x
+
+# values = [2, 3, 5, 7, 11, 13, 17, 19, 23, 29] # или любой другой список
+# transormed_values = list(map(lambda x: x, values))
+
+
+# print(transormed_values == values
+
+
+
+# Задача №49. Решение в группах
+# Планеты вращаются вокруг звезд по эллиптическим орбитам.
+# Назовем самой далекой планетой ту, орбита которой имеет
+# самую большую площадь. Напишите функцию
+# find_farthest_orbit(list_of_orbits), которая среди списка орбит
+# планет найдет ту, по которой вращается самая далекая
+# планета. Круговые орбиты не учитывайте: вы знаете, что у
+# вашей звезды таких планет нет, зато искусственные спутники
+# были были запущены на круговые орбиты. Результатом
+# функции должен быть кортеж, содержащий длины полуосей
+# эллипса орбиты самой далекой планеты. Каждая орбита
+# представляет из себя кортеж из пары чисел - полуосей ее
+# эллипса. Площадь эллипса вычисляется по формуле S = pi*a*b,
+# где a и b - длины полуосей эллипса. При решении задачи
+# используйте списочные выражения. Подсказка: проще всего
+# будет найти эллипс в два шага: сначала вычислить самую
+# большую площадь эллипса, а затем найти и сам эллипс,
+# имеющий такую площадь. Гарантируется, что самая далекая
+# планета ровно одна
+# Пример ввода и вывода данных представлены на
+# следующем слайде
+
+# orbits = [(1, 3), (2.5, 10), (7, 2), (6, 6), (4, 3)]
+
+# def find_farthest_orbit(list_orbits):
+#     max_square = 0
+#     max_element = list_orbits[0]
+#     for element in list_orbits:
+#         for j in range(1):
+#             if element[j] != element[j + 1] and max_square < element[j] * element[j + 1]:
+#                 max_square = element[j] * element[j + 1]
+#                 max_element = element
+#     return max_element            
+
+# print(*find_farthest_orbit(orbits))
+
+# Через словариL:
+# def ﬁnd_farthest_orbit(list_of_orbits):
+#     dict_s_orbits = {3.14 * orbit[0] * orbit[1]: orbit for orbit in list_of_orbits if orbit[0] != orbit[1]}
+#     return dict_s_orbits[max(dict_s_orbits)]
+
+# Через лямбда функцию:
+# def ﬁnd_farthest_orbit(list_of_orbits):
+#     return max(filter(lambda x: x[0] != x[1], list_of_orbits), key=lambda x: 3.14 * x[0] * x[1])
+
+
 #   HOMEWORK №1
 # Напишите функцию print_operation_table(operation, num_rows, num_columns), 
 # которая принимает в качестве аргумента функцию, вычисляющую элемент по номеру строки и столбца. 
@@ -41,34 +122,34 @@
 # Количество фраз должно быть больше одной!
 
 
-stroka = 'пара-ра-рам парарарам'
-new_stroka = stroka.split()
-letters = 'ауоыиэяюёеАУОЫИЭЯЮЁЕ'
-count = 0
-calc = 0
-string = ''
-check = []
+# stroka = 'пара-ра-рам парарарам'
+# new_stroka = stroka.split()
+# letters = 'ауоыиэяюёеАУОЫИЭЯЮЁЕ'
+# count = 0
+# calc = 0
+# string = ''
+# check = []
 
-if len(new_stroka) < 2:
-    print('Количество фраз должно быть больше одной!')
-else:
-    for i in range(len(new_stroka)):
-        string = new_stroka[i]
-        for j in string:
-            if j in letters:
-                count += 1
-        check.append(count)
-        count = 0
+# if len(new_stroka) < 2:
+#     print('Количество фраз должно быть больше одной!')
+# else:
+#     for i in range(len(new_stroka)):
+#         string = new_stroka[i]
+#         for j in string:
+#             if j in letters:
+#                 count += 1
+#         check.append(count)
+#         count = 0
     
-    for i in range(len(check) - 1):
-        if check[i] != check[i+1]:
-            print('Пам парам')
-            break
-        else:
-            calc += 1
+#     for i in range(len(check) - 1):
+#         if check[i] != check[i+1]:
+#             print('Пам парам')
+#             break
+#         else:
+#             calc += 1
         
-    if calc == (len(check)-1):
-        print('Парам пам-пам')
+#     if calc == (len(check)-1):
+#         print('Парам пам-пам')
             
             
 
