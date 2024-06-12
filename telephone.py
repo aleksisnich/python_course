c0155d5770411a5db4fc2c26f8fa5a01aeeabdec
aleksis <nichik_a@bk.ru>
2024-06-13 01:38:15 +0300

    add to telephone book

---
+++ telephone.py
@@ -0,0 +1,111 @@
+from tabulate import tabulate
+
+def work_with_phonebook():
+	
+    choice = show_menu()
+
+    phone_book = read_txt('phon.txt')
+    
+    while (choice != 7):
+
+        if choice == 1:
+            print_result(phone_book)
+        elif choice == 2:
+            last_name=input('lastname: ')
+            print(find_by_lastname(phone_book,last_name))
+        elif choice == 3:
+            last_name = input('lastname: ')
+            new_number = input('new  number: ')
+            print(change_number(phone_book,last_name,new_number))  	
+        elif choice==4:
+            lastname=input('lastname: ')
+            print(delete_by_lastname(phone_book,lastname))
+        elif choice==5:
+            number=input('number: ')
+            print(find_by_number(phone_book,number))
+        elif choice==6:
+            user_data=input('new data: ')
+            add_user(phone_book,user_data)
+            write_txt('phon.txt',phone_book)
+
+
+        choice=show_menu()
+
+
+def show_menu():
+    print("\nВыберите необходимое действие:\n"
+          "1. Показать весь справочник\n"
+          "2. Поиск абонента по фамилии\n"
+          "3. Поиск абонента по номеру телефона\n"
+          "4. Добавить абонента в справочник\n"
+          "5. Сохранить справочник в текстовом формате\n"
+          "6. Закончить работу")
+    choice = int(input())
+    return choice
+
+
+def read_txt(filename): 
+
+    phonebook=[]
+
+    fields=['ФАМИЛИЯ', 'ИМЯ', 'ТЕЛЕФОН', 'ОПИСАНИЕ']
+
+    with open(filename,'r',encoding='utf-8') as phb:
+        for line in phb:
+            record = dict(zip(fields, line.split(',')))
+            phonebook.append(record)
+    return phonebook
+
+
+
+def write_txt(filename, phone_book):
+
+    with open(filename,'w',encoding='utf-8') as phout:
+
+        for i in range(len(phone_book)):
+
+            s=''
+            for v in phone_book[i].values():
+
+                s = s + v + ','
+
+            phout.write(f'{s[:-1]}\n')
+            
+def print_result(phone_book):
+    fields = ['ФАМИЛИЯ', 'ИМЯ', 'ТЕЛЕФОН', 'ОПИСАНИЕ']
+    # print(*fields,'\n')
+    # for val in phone_book:
+    #     for i in fields:
+    #         print(val.get(i), end = '')
+    #     print('\n')
+    print(tabulate(phone_book, headers = "keys", tablefmt = "pipe"))
+
+# def find_by_lastname(phone_book, last_name):
+    
+    
+
+work_with_phonebook()
+
+    
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
