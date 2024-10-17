44046beaa7ed436ccb3ac58a18f965def5150afe
aleksis <nichik_a@bk.ru>
2024-10-18 01:27:18 +0300

    modified bot

--- telegram_bot.py
+++ telegram_bot.py
@@ -2,33 +2,33 @@ from random import *
 import json
 import requests
 import telebot
-films = []
+books = []
 API_URL = 'https://7012.deeppavlov.ai/model'
 API_TOKEN = '7002491636:AAG04JvzXm0cx2YSgwXrnYyocAG0vaXXSBo'
 bot = telebot.TeleBot(API_TOKEN)
 
 @bot.message_handler(commands=['start']) #декоратор хз что это - связываем команду старт с ботом
 def start_message(message):
-    films.append("Матрица")
-    films.append("Солярис")
-    films.append("Властелин колец")
-    films.append("Техасская резня бензопилой")
-    films.append("Санта Барбара")
-    bot.send_message(message.chat.id,"Фильмотека была загружена по умолчанию!")
+    books.append("Над пропастью во ржи")
+    books.append("Чапаев и пустота")
+    books.append("Война и мир")
+    books.append("Заводной апельсин")
+    books.append("Мастер и Маргарита")
+    bot.send_message(message.chat.id,"Библиотека была загружена по умолчанию!")
 
 @bot.message_handler(commands=['all'])
 def show_all(message):
     try:
-        bot.send_message(message.chat.id,"Вот список фильмов")
-        bot.send_message(message.chat.id, ", ".join(films))
+        bot.send_message(message.chat.id,"Вот список книг")
+        bot.send_message(message.chat.id, ", ".join(books))
     except:
-        bot.send_message(message.chat.id, "Фильмов нема")
+        bot.send_message(message.chat.id, "Книг нема")
 
 @bot.message_handler(commands=['save'])
 def save_all(message):
-    with open("films.json", "w", encoding="utf-8") as fh:
-        fh.write(json.dumps(films, ensure_ascii=False))
-    bot.send_message(message.chat.id,"Наша фильмотека была успешно сохранена в файле films.json")
+    with open("books.json", "w", encoding="utf-8") as fh:
+        fh.write(json.dumps(books, ensure_ascii=False))
+    bot.send_message(message.chat.id,"Наша библиотека была успешно сохранена в файле books.json")
 
 @bot.message_handler(commands=['wiki'])
 def wiki(message):
@@ -43,7 +43,10 @@ def wiki(message):
 
 @bot.message_handler(content_types=['text'])
 def get_text_messages(message):
-    if "дела" in message.text.lower():
-        bot.send_message(message.chat.id, "Все супер, у тебя как?")
+    if "привет" in message.text.lower():
+        bot.send_message(message.chat.id, "Привет, я книжный бот и могу подсказать тебе список интересных книг:"
+                                          "- Для старта напиши /start"
+                                          "- Для просмотра списка книг напиши /all"
+                                          "- Для поиска в вики напиши /wiki")
 
 bot.polling()
