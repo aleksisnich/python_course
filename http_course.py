06a7abb87be301bf74530147bfdcd8b99c98ce8d
aleksis <nichik_a@bk.ru>
2024-11-21 00:03:00 +0300

    created http_course.py

---
+++ http_course.py
@@ -0,0 +1,27 @@
+import requests
+import json
+
+url = "http://openlibrary.org/search.json"
+
+subject = "Artificial intelligence"
+params = {
+    "subject" : subject,
+    "limit" : 10
+}
+response = requests.get(url, params=params)
+
+if response.status_code == 200:
+    print("Успешный запрос API")
+else:
+    print("Запрос API отклонен с кодом состояния: ", response.status_code)
+
+# print(response.text)
+
+# ввиде словаря
+data = json.loads(response.text)
+books = data["docs"]
+for book in books:
+    print("Title: ", book["title"])
+    print("Author: ", *book["author_name"])
+    print("Subject: ", *book["subject"])
+    print("\n")
