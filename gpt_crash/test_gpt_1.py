9300533d160888183560757d8722202501cec939
aleksis <nichik_a@bk.ru>
2025-02-13 19:15:17 +0300

    created project with gpt4

---
+++ gpt_crash/test_gpt_1.py
@@ -0,0 +1,9 @@
+from g4f.client import Client
+
+client = Client()
+response = client.chat.completions.create(
+    model="gpt-4o-mini",
+    messages=[{"role": "user", "content": "Что такое DeepSeek"}],
+    web_search=False
+)
+print(response.choices[0].message.content)
